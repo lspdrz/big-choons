ba63b0df6419b678146a24d4890daac113a01fdc
Luis Pedraza <luis@lspdrz.com>
2020-09-22 21:37:58 -0400

    added initial prisma models

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { PrismaClient } from "@prisma/client";
 
-const IndexPage = () => (
+const IndexPage = ({ users }) => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
     <p>
@@ -9,7 +10,23 @@ const IndexPage = () => (
         <a>About</a>
       </Link>
     </p>
+    <ul>
+      {users.map((user) => (
+        <li>Name: {user.name}</li>
+      ))}
+    </ul>
   </Layout>
-)
+);
 
-export default IndexPage
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      name: true,
+    },
+  });
+  return { props: { users } };
+};
+
+export default IndexPage;

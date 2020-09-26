c1a94d5741a7ff6243f77e51de354fab6fc4fd82
Roby Saavedra <roby.saavedra@gmail.com>
2020-09-26 19:54:22 -0400

    Added Icons

---
+++ components/icons/Play.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Play() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7 5.86852C7 5.06982 7.89015 4.59343 8.5547 5.03647L17.7519 11.168C18.3457 11.5638 18.3457 12.4362 17.7519 12.8321L8.5547 18.9635C7.89014 19.4066 7 18.9302 7 18.1315V5.86852Z"
+        fill="white"
+      />
+    </svg>
+  );
+}

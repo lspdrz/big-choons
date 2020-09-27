2fc0877e8cac552d054b2ebaedcd472c6655fe48
Roby Saavedra <roby.saavedra@gmail.com>
2020-09-26 22:57:14 -0400

    Styled the player component

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import Link from "next/link";
+import ExternalLink from "./icons/ExternalLink";
+import Volume from "./icons/Volume";
+import SkipBack from "./icons/SkipBack";
+import Play from "./icons/Play";
+import SkipNext from "./icons/SkipNext";
 
 const Player = () => (
-  <section className="w-full border-t bg-white h-20 p-4 fixed bottom-0">
+  <section className="w-full border-t bg-white h-24 p-4 fixed bottom-0">
     <div className="flex flex-row justify-between w-full h-full">
       {/* PLAYER CONTROLS */}
       <div className="flex flex-row justify-self-start items-center">
-        <span className="h-8 w-8 bg-pink-500 mr-2" />
-        <span className="h-12 w-12 bg-pink-800 mr-2" />
-        <span className="h-8 w-8 bg-pink-500 mr-2" />
+        <button className="p-2 mr-2">
+          <SkipBack />
+        </button>
+        <button className="p-4 mr-2 rounded-full bg-green-600">
+          <Play />
+        </button>
+        <button className="p-2 mr-2">
+          <SkipNext />
+        </button>
       </div>
       {/* SONG INFO */}
       <div className="group flex flex-row">
@@ -17,7 +28,7 @@ const Player = () => (
           className="w-16 h-16 bg-yellow-300 mr-2 flex justify-center content-center"
           style={{
             backgroundImage:
-              "url('https://i.ytimg.com/vi/Zse87KUaAMo/hqdefault.jpg')",
+              "url('http://img.youtube.com/vi/othRjM828bk/0.jpg')",
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center",
             backgroundSize: "cover",
@@ -30,12 +41,20 @@ const Player = () => (
         </span>
         <div className="flex flex-col content-center">
           <p className="text-xs text-gray-600">
-            Shared by <Link href="#">User Name</Link>
+            Shared by <Link href="#">User Name</Link> (Expires in 22:42:42)
           </p>
           <p className="text-lg hover:underline">
-            <Link href="#">►The Heinz Kiessling Orchestra◄ - Remember Rio</Link>
+            <Link href="#">The Heinz Kiessling Orchestra - Remember Rio</Link>
           </p>
         </div>
+      </div>
+      <div className="flex flex-row">
+        <button className="p-2">
+          <ExternalLink />
+        </button>
+        <button className="p-2 ml-4">
+          <Volume />
+        </button>
       </div>
     </div>
   </section>

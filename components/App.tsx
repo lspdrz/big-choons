cd09df757125f2ef00a4cb7aadc12704d60aaae0
Luis Pedraza <luis@lspdrz.com>
2020-11-18 16:59:55 +0300

    added dummy authentication boolean and login page

--- components/App.tsx
+++ components/App.tsx
@@ -5,13 +5,15 @@ import Modals from "./modals/Modals";
 import { AppProvider } from "./AppContext";
 import TrackFeed from "./TrackFeed";
 import AboutButton from "./AboutButton";
+import LoginPage from "./LoginPage";
 
+const userAuthenticated = false
 const App = () => {
   return (
     <AppProvider>
       <Modals />
       <Layout title="Big Choonz">
-        <TrackFeed />
+        {userAuthenticated ? <TrackFeed /> : <LoginPage />}
       </Layout>
       <AboutButton />
     </AppProvider>
---
+++ components/LoginPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import Link from 'next/link'
+
+const LoginPage = () => (
+  <div>
+      <p>I am the login page</p>
+      </div>
+)
+
+export default LoginPage

2458c86ef9043c3b5353b9c3d2c313df42074a43
Roby Saavedra <roby.saavedra@gmail.com>
2020-11-22 15:05:55 -0500

    styling logout modal

--- components/modals/LogoutModal.tsx
+++ components/modals/LogoutModal.tsx
@@ -14,10 +14,13 @@ const LogoutModal = () => {
     return (
         <BaseModal title="Log out">
             <div className="max-w-2xl flex flex-col">
-                <div className="flex items-center justify-between focus:border-b focus:border-purple-500">
+                <div className="p-6 flex flex-col items-center focus:border-b focus:border-purple-500">
                     <p>Do you really want to log out?</p>
-                    <button onClick={() => closeModal()}>No</button>
-                    <button onClick={() => logout()}>Yes</button>
+                    <div className="w-full flex justify-end">
+
+                    <button className="bg-white shadow p-4 mx-auto mt-8 border border-gray-300 hover:bg-gray-200 focus:bg-gray-100 focus:ring-2" onClick={() => closeModal()}>Nevermind</button>
+                    <button className="bg-white shadow p-4 mx-auto mt-8 border border-gray-300 hover:bg-gray-200 focus:bg-gray-100 focus:ring-2" onClick={() => logout()}>Yes</button>
+                    </div>
                 </div>
             </div>
         </BaseModal>

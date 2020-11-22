f8d5b82c447846e682bd495b6c292a0b884c2746
Roby Saavedra <roby.saavedra@gmail.com>
2020-11-22 16:09:17 -0500

    making logout modal responsive

--- components/modals/LogoutModal.tsx
+++ components/modals/LogoutModal.tsx
@@ -14,11 +14,11 @@ const LogoutModal = () => {
     return (
         <BaseModal title="Log out">
             <div className="max-w-2xl flex flex-col">
-                <div className="p-8 flex flex-col focus:border-b focus:border-purple-500">
-                    <div className="w-full flex items-center justify-between">
+                <div className="p-8 focus:border-b focus:border-purple-500">
+                    <div className="w-full flex items-center justify-between sm:flex sm:flex-col">
 
                     <p>Please confirm you want to log out</p>
-                    <button className="bg-red-700 text-white p-4 hover:bg-red-900 focus:bg-gray-100 focus:ring-2" onClick={() => logout()}>Log out</button>
+                    <button className="bg-red-700 text-white p-4 hover:bg-red-900 focus:bg-gray-100 focus:ring-2 sm:w-full" onClick={() => logout()}>Log out</button>
                     </div>
                 </div>
             </div>

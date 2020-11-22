edb40c9c32bbac743686e8eae196f20d47ae37fe
Roby Saavedra <roby.saavedra@gmail.com>
2020-11-22 16:20:21 -0500

    fix

--- components/modals/LogoutModal.tsx
+++ components/modals/LogoutModal.tsx
@@ -15,10 +15,10 @@ const LogoutModal = () => {
         <BaseModal title="Log out">
             <div className="max-w-2xl flex flex-col">
                 <div className="p-8 focus:border-b focus:border-purple-500">
-                    <div className="w-full flex items-center justify-between sm:flex sm:flex-col">
+                    <div className="w-full flex items-center justify-between flex-col">
 
                     <p>Please confirm you want to log out</p>
-                    <button className="bg-red-700 text-white hover:bg-red-900 focus:bg-gray-100 focus:ring-2 w-full sm:p-4 sm:mt-8" onClick={() => logout()}>Log out</button>
+                    <button className="bg-red-700 text-white hover:bg-red-900 focus:bg-gray-100 focus:ring-2 w-full mt-8" onClick={() => logout()}>Log out</button>
                     </div>
                 </div>
             </div>

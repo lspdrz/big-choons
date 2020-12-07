0347099f7d0ce61089cdd8a4993e1c236f434d86
Luis Pedraza <luis@lspdrz.com>
2020-12-07 13:40:56 +0300

    added jest and initial test

---
+++ pages/index.test.tsx
@@ -0,0 +1,9 @@
+import renderer from 'react-test-renderer'
+import IndexPage from './index'
+
+describe('Index page', () => {
+  it('should match the snapshot', () => {
+    const tree = renderer.create(<IndexPage />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
